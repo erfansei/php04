ffeae785a0493b2022bd82ec7e7fc2619247d630
erfansei <erfan.saleki@gmail.com>
2023-11-25 13:35:46 +0330

    update02

--- function.php
+++ function.php
@@ -1,18 +1,18 @@
 <?php
 function renderProducts($nameP,$desc,$st,$cl)
 {
-    $html = '<div class="bg-slate-200">';
+    $html = '<div class="bg-slate-200 border-rose-500">';
     $html .= '<div class="text-center font-sans">';
     $html .= "<h1 class='font-bold'>{$nameP}</h1>";
     $html .= '</div>';
     $html .= '<div class="text-justify">';
     $html .= "<p>توضیحات : {$desc}</p>";
     $html .= '</div>';
-    $html .= '<div class="">';
+    $html .= '<div class="text-center">';
     $html .= "<br><span>موجودی :{$st}</span>";
     $html .= '</div>';
-    $html .= '<div class="flex">';
-    $html .= "<span>رنگ ها:{$cl}</span>";
+    $html .= '<div class="text-center">';
+    $html .= "<p>رنگ ها :{$cl}</p>";
     $html .= '</div>';
     $html .= '</div>';
 
